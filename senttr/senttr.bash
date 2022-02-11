dfdd9e4382a3496448093d49664db176f2f47b8e
Eleftheria Tsipidi <etsipidi@gmail.com>
2022-02-11 15:12:29 +0100

    Update paths in script

--- senttr/senttr.bash
+++ senttr/senttr.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
-MAIN_PATH=""
-BERT_PATH=""
-DATA_PATH=""
+MAIN_PATH="/home/etsipidi/Documents/Parsing/G2GTr/senttr/embeddings"
+BERT_PATH="/home/etsipidi/Documents/Parsing/G2GTr/senttr/tempbert/"
+DATA_PATH="/home/etsipidi/Documents/Parsing/G2GTr/senttr/data"
 
 python run.py train --lr 1e-5 -w 0.001 --modelname senttr --batch_size 40 --buckets 10 \
          --ftrain $DATA_PATH/train.conll \
-         --ftrain_seq $DATA_PATH/train.seq \
+         --ftrain_seq $DATA_PATH/oracle_eager.seq \
          --ftest $DATA_PATH/test.conll \
          --fdev $DATA_PATH/dev.conll \
          --bert_path $BERT_PATH --punct --n_attention_layer 6 --epochs 12 --act_thr 280 \

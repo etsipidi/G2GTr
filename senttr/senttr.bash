7ccb297fdd4eef0f812052417be8d178e7e8d5f6
Eleftheria Tsipidi <etsipidi@gmail.com>
2022-02-14 11:42:16 +0100

    Modify scripts for arceager

--- senttr/senttr.bash
+++ senttr/senttr.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
-MAIN_PATH="/home/etsipidi/Documents/Parsing/G2GTr/senttr/embeddings"
+MAIN_PATH="/home/etsipidi/Documents/Parsing/G2GTr/senttr/embeddings/"
 BERT_PATH="/home/etsipidi/Documents/Parsing/G2GTr/senttr/tempbert/"
-DATA_PATH="/home/etsipidi/Documents/Parsing/G2GTr/senttr/data"
+DATA_PATH="/home/etsipidi/Documents/Parsing/G2GTr/senttr/data/projective/"
 
-python run.py train --lr 1e-5 -w 0.001 --modelname senttr --batch_size 40 --buckets 10 \
+python run.py train --lr 1e-5 -w 0.001 --modelname senttr --batch_size 32 --buckets 5 \
          --ftrain $DATA_PATH/train.conll \
-         --ftrain_seq $DATA_PATH/train.seq \
+         --ftrain_seq $DATA_PATH/oracle_eager.seq \
          --ftest $DATA_PATH/test.conll \
          --fdev $DATA_PATH/dev.conll \
-         --bert_path $BERT_PATH --punct --n_attention_layer 6 --epochs 12 --act_thr 280 \
-         --main_path $MAIN_PATH
+         --bert_path $BERT_PATH --punct --n_attention_layer 6 --epochs 12 --act_thr 210 \
+         --main_path $MAIN_PATH --device 0
--- senttr/senttr_g2g.bash
+++ senttr/senttr_g2g.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
-MAIN_PATH=""
-BERT_PATH=""
-DATA_PATH=""
+MAIN_PATH="/home/etsipidi/Documents/Parsing/G2GTr/senttr/embeddings/"
+BERT_PATH="/home/etsipidi/Documents/Parsing/G2GTr/senttr/tempbert/"
+DATA_PATH="/home/etsipidi/Documents/Parsing/G2GTr/senttr/data/"
 
-python run.py train --lr 1e-5 --lr2 1e-4 -w 0.001 --modelname senttr_g2g --batch_size 40 --buckets 10 \
+python run.py train --lr 1e-5 --lr2 1e-4 -w 0.001 --modelname senttr_g2g --batch_size 32 --buckets 5 \
          --ftrain $DATA_PATH/train.conll \
-         --ftrain_seq $DATA_PATH/train.seq \
+         --ftrain_seq $DATA_PATH/oracle_eager.seq \
          --ftest $DATA_PATH/test.conll \
          --fdev $DATA_PATH/dev.conll \
          --bert_path $BERT_PATH --punct --n_attention_layer 6 --epochs 12 \
-         --input_graph --act_thr 210 --use_two_opts --main_path $MAIN_PATH
+         --input_graph --act_thr 180 --use_two_opts --main_path $MAIN_PATH --device 0

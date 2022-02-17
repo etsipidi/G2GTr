6a9946f01c23432183494dcc26e38adf7bf4fba9
etsipidi <etsipidi@gmail.com>
2022-02-17 15:53:00 +0100

    Modify training script for g2g

--- senttr/senttr_g2g.bash
+++ senttr/senttr_g2g.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
-MAIN_PATH="/home/etsipidi/Documents/Parsing/G2GTr/senttr/embeddings/"
+MAIN_PATH="/home/etsipidi/Documents/Parsing/G2GTr/senttr/"
 BERT_PATH="/home/etsipidi/Documents/Parsing/G2GTr/senttr/tempbert/"
-DATA_PATH="/home/etsipidi/Documents/Parsing/G2GTr/senttr/data/"
+DATA_PATH="/home/etsipidi/Documents/Parsing/G2GTr/senttr/data/ud_v1_3/"
 
-python run.py train --lr 1e-5 --lr2 1e-4 -w 0.001 --modelname senttr_g2g --batch_size 32 --buckets 5 \
+python run.py train --lr 1e-5 --lr2 1e-4 -w 0.001 --modelname senttr_g2g_ae --batch_size 40 --buckets 10 \
          --ftrain $DATA_PATH/train.conll \
          --ftrain_seq $DATA_PATH/oracle_eager.seq \
          --ftest $DATA_PATH/test.conll \
          --fdev $DATA_PATH/dev.conll \
-         --bert_path $BERT_PATH --punct --n_attention_layer 6 --epochs 12 \
-         --input_graph --act_thr 180 --use_two_opts --main_path $MAIN_PATH --device 0
+         --bert_path $BERT_PATH --punct --n_attention_layer 7 --epochs 12 \
+         --model model/ --vocab model/ \
+         --input_graph --act_thr 210 --use_two_opts --main_path $MAIN_PATH --device 0 -t 8
